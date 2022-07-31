9a754578c09931f0dfa80094877c4c23bd5e2250
Arlo O'Keeffe <tiger@k5d.de>
2022-07-31 21:25:57 +0200

    update graalvm to 22.2.0

--- src/main/java/io/github/arlol/chorito/chores/GitHubActionChore.java
+++ src/main/java/io/github/arlol/chorito/chores/GitHubActionChore.java
@@ -52,7 +52,7 @@ public class GitHubActionChore {
 					.stream()
 					.map(s -> {
 						if (s.startsWith("  GRAALVM_VERSION: ")) {
-							return "  GRAALVM_VERSION: 22.1.0";
+							return "  GRAALVM_VERSION: 22.2.0";
 						}
 						return s;
 					})
