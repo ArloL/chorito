70c8b69721aa606a1d97ea15bfde0a1ac664513e
Arlo O'Keeffe <tiger@k5d.de>
2022-11-06 09:24:11 +0100

    add MavenWrappeChoreTest

---
+++ src/test/java/io/github/arlol/chorito/chores/MavenWrapperChoreTest.java
@@ -0,0 +1,59 @@
+package io.github.arlol.chorito.chores;
+
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.github.arlol.chorito.tools.FakeSilentProcessBuilder;
+import io.github.arlol.chorito.tools.FileSystemExtension;
+import io.github.arlol.chorito.tools.FilesSilent;
+
+public class MavenWrapperChoreTest {
+
+	@RegisterExtension
+	final FileSystemExtension extension = new FileSystemExtension();
+
+	@Test
+	public void testWithNothing() {
+		new MavenWrapperChore(extension.choreContext()).doit();
+	}
+
+	@Test
+	void testNada() throws Exception {
+		var context = extension.choreContext()
+				.toBuilder()
+				.processBuilderFactory(FakeSilentProcessBuilder.factory())
+				.build();
+		new MavenWrapperChore(context).doit();
+	}
+
+	@Test
+	void testCreateMavenWrapper() throws Exception {
+		var context = extension.choreContext()
+				.toBuilder()
+				.processBuilderFactory(
+						FakeSilentProcessBuilder.factory(this::fakeMavenWrapper)
+				)
+				.build();
+		Path pom = context.resolve("pom.xml");
+		FilesSilent.writeString(pom, "");
+
+		new MavenWrapperChore(context).doit();
+	}
+
+	private void fakeMavenWrapper() {
+		var context = extension.choreContext();
+		FilesSilent.writeString(context.resolve("mvnw"), "");
+		FilesSilent.writeString(context.resolve("mvnw.cmd"), "");
+		FilesSilent.writeString(
+				context.resolve(".mvn/wrapper/maven-wrapper.jar"),
+				""
+		);
+		FilesSilent.writeString(
+				context.resolve(".mvn/wrapper/maven-wrapper.properties"),
+				""
+		);
+	}
+
+}
