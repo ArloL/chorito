896cac3209e393f51d53bc3cfe55e91a74479582
Arlo O'Keeffe <tiger@k5d.de>
2022-11-07 21:53:39 +0100

    add JitpackChore

---
+++ src/main/java/io/github/arlol/chorito/chores/JitpackChore.java
@@ -0,0 +1,31 @@
+package io.github.arlol.chorito.chores;
+
+import java.nio.file.Path;
+
+import io.github.arlol.chorito.tools.ChoreContext;
+import io.github.arlol.chorito.tools.FilesSilent;
+
+public class JitpackChore {
+
+	private static final String JAVA_JITPACK = """
+			jdk:
+			- openjdk17
+			install:
+			- ./mvnw --batch-mode install -DskipTests -Dsha1="${GIT_COMMIT}" -Drevision="${VERSION}"
+			""";
+
+	private ChoreContext context;
+
+	public JitpackChore(ChoreContext context) {
+		this.context = context;
+	}
+
+	public void doit() {
+		Path pom = context.resolve("pom.xml");
+		Path jitpack = context.resolve("jitpack.yml");
+		if (FilesSilent.exists(pom) && !FilesSilent.exists(jitpack)) {
+			FilesSilent.writeString(jitpack, JAVA_JITPACK);
+		}
+	}
+
+}
---
+++ src/test/java/io/github/arlol/chorito/chores/JitpackChoreTest.java
@@ -0,0 +1,48 @@
+package io.github.arlol.chorito.chores;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.github.arlol.chorito.tools.FileSystemExtension;
+import io.github.arlol.chorito.tools.FilesSilent;
+
+public class JitpackChoreTest {
+
+	private static final String EXPECT_JITPACK = """
+			jdk:
+			- openjdk17
+			install:
+			- ./mvnw --batch-mode install -DskipTests -Dsha1="${GIT_COMMIT}" -Drevision="${VERSION}"
+			""";
+
+	@RegisterExtension
+	final FileSystemExtension extension = new FileSystemExtension();
+
+	@Test
+	public void testWithNothing() {
+		Path jitpack = extension.root().resolve("jitpack.yml");
+
+		new JitpackChore(extension.choreContext()).doit();
+
+		assertFalse(FilesSilent.exists(jitpack));
+	}
+
+	@Test
+	public void test() throws Exception {
+		Path jitpack = extension.root().resolve("jitpack.yml");
+		Path pom = extension.root().resolve("pom.xml");
+		FilesSilent.writeString(pom, "<project />");
+
+		new JitpackChore(extension.choreContext()).doit();
+
+		assertTrue(FilesSilent.exists(jitpack));
+		assertThat(FilesSilent.readString(jitpack)).isEqualTo(EXPECT_JITPACK);
+	}
+
+}
