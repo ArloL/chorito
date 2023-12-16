9f949492d23ea3118cf62613761c641416d9bff0
Arlo O'Keeffe <tiger@k5d.de>
2023-12-16 18:16:22 +0100

    add ClassPathFiles.newInputStream and .newBufferedReader methods

--- src/main/java/io/github/arlol/chorito/tools/ClassPathFiles.java
+++ src/main/java/io/github/arlol/chorito/tools/ClassPathFiles.java
@@ -2,9 +2,13 @@ package io.github.arlol.chorito.tools;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.io.UncheckedIOException;
+import java.nio.charset.CharsetDecoder;
 
 public abstract class ClassPathFiles {
 
@@ -12,12 +16,21 @@ public abstract class ClassPathFiles {
 	}
 
 	public static String readString(String path) {
-		try (InputStream stream = ClassPathFiles.class
-				.getResourceAsStream(path)) {
+		try (InputStream stream = newInputStream(path)) {
 			return new String(stream.readAllBytes(), UTF_8);
 		} catch (IOException e) {
 			throw new UncheckedIOException(e);
 		}
+	}
+
+	public static InputStream newInputStream(String path) {
+		return ClassPathFiles.class.getResourceAsStream(path);
+	}
+
+	public static BufferedReader newBufferedReader(String path) {
+		CharsetDecoder decoder = UTF_8.newDecoder();
+		Reader reader = new InputStreamReader(newInputStream(path), decoder);
+		return new BufferedReader(reader);
 	}
 
 }
