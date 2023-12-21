e3967ba4f206bb35c12c2bd48da3d2338f6c4f08
Arlo O'Keeffe <tiger@k5d.de>
2023-12-21 11:46:17 +0100

    add a test script for the final executable

--- src/main/java/io/github/arlol/chorito/chores/GitHubActionChore.java
+++ src/main/java/io/github/arlol/chorito/chores/GitHubActionChore.java
@@ -46,6 +46,8 @@ public class GitHubActionChore implements Chore {
 		if (!main.hasJob("version")) {
 			return;
 		}
+		updateTestExecutable(context);
+
 		var currentMain = new GitHubActionsWorkflowFile(
 				ClassPathFiles
 						.readString("/github-settings/workflows/main.yaml")
@@ -54,6 +56,27 @@ public class GitHubActionChore implements Chore {
 		main.setJob("linux", currentMain.getJob("linux"));
 		main.setJob("windows", currentMain.getJob("windows"));
 		FilesSilent.writeString(mainYaml, main.asString());
+	}
+
+	private void updateTestExecutable(ChoreContext context) {
+		List<String> templateLines = ClassPathFiles
+				.readAllLines("/test-executable.sh");
+		templateLines = templateLines.subList(
+				0,
+				templateLines.indexOf("# add custom tests here:") + 1
+		);
+		Path testExectuable = context
+				.resolve("src/main/resources/test-executable.sh");
+		if (FilesSilent.exists(testExectuable)) {
+			List<String> testExecutableLines = FilesSilent
+					.readAllLines(testExectuable);
+			testExecutableLines = testExecutableLines.subList(
+					testExecutableLines.indexOf("# add custom tests here:") + 1,
+					testExecutableLines.size()
+			);
+			templateLines.addAll(testExecutableLines);
+		}
+		FilesSilent.write(testExectuable, templateLines, "\n");
 	}
 
 	private void updateDebugSteps(ChoreContext context) {
