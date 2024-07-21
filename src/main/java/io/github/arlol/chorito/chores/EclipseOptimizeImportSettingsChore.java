c0575ba7d2820f45a70fd6335edf751a5b07ca01
Arlo O'Keeffe <tiger@k5d.de>
2024-07-21 22:32:32 +0200

    update org.eclipse.jdt.ui.prefs and add chore to keep em consistent

---
+++ src/main/java/io/github/arlol/chorito/chores/EclipseOptimizeImportSettingsChore.java
@@ -0,0 +1,80 @@
+package io.github.arlol.chorito.chores;
+
+import java.io.StringReader;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import io.github.arlol.chorito.tools.ChoreContext;
+import io.github.arlol.chorito.tools.ClassPathFiles;
+import io.github.arlol.chorito.tools.FilesSilent;
+import io.github.arlol.chorito.tools.MyPaths;
+import io.github.arlol.chorito.tools.PropertiesSilent;
+
+public class EclipseOptimizeImportSettingsChore implements Chore {
+
+	@Override
+	public ChoreContext doit(ChoreContext context) {
+		AtomicBoolean changed = new AtomicBoolean();
+		context.textFiles()
+				.stream()
+				.filter(file -> file.endsWith("pom.xml"))
+				.map(MyPaths::getParent)
+				.forEach(pomDir -> {
+					String templateJdtUiPrefs = ClassPathFiles.readString(
+							"eclipse-settings/org.eclipse.jdt.ui.prefs"
+					);
+
+					Path jdtUiPrefs = pomDir
+							.resolve(".settings/org.eclipse.jdt.ui.prefs");
+					if (!FilesSilent.exists(jdtUiPrefs)) {
+						FilesSilent.touch(jdtUiPrefs);
+						changed.set(true);
+					}
+
+					Map<String, String> jdtUiPrefsMap = new PropertiesSilent()
+							.load(jdtUiPrefs)
+							.toMap();
+					int prefsHashCodeAtStart = jdtUiPrefsMap.hashCode();
+
+					jdtUiPrefsMap.put("eclipse.preferences.version", "1");
+
+					new PropertiesSilent()
+							.load(new StringReader(templateJdtUiPrefs))
+							.toMap()
+							.entrySet()
+							.stream()
+							.filter(
+									e -> e.getKey()
+											.startsWith("org.eclipse.jdt.ui.")
+							)
+							.forEach(
+									e -> jdtUiPrefsMap
+											.put(e.getKey(), e.getValue())
+							);
+
+					// update the file only if something changed
+					// otherwise this triggers a rebuild in Eclipse and
+					// Eclipse-based tools (VS Code)
+					if (jdtUiPrefsMap.hashCode() != prefsHashCodeAtStart) {
+
+						List<String> propertyList = jdtUiPrefsMap.entrySet()
+								.stream()
+								.map(
+										e -> e.getKey() + "="
+												+ e.getValue()
+														.replace(":", "\\:")
+								)
+								.toList();
+
+						FilesSilent.write(jdtUiPrefs, propertyList, "\n");
+					}
+				});
+		if (changed.get()) {
+			return context.refresh();
+		}
+		return context;
+	}
+
+}
