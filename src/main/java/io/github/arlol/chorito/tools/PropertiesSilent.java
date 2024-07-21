ff992157da9289be7582d61eb4b824fb6d133878
Arlo O'Keeffe <tiger@k5d.de>
2024-07-21 13:43:45 +0200

    add PropertiesSilent.load(Reader)

--- src/main/java/io/github/arlol/chorito/tools/PropertiesSilent.java
+++ src/main/java/io/github/arlol/chorito/tools/PropertiesSilent.java
@@ -2,6 +2,7 @@ package io.github.arlol.chorito.tools;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,6 +17,15 @@ public class PropertiesSilent {
 	public PropertiesSilent load(Path path) {
 		try (InputStream inputStream = Files.newInputStream(path)) {
 			properties.load(inputStream);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+		return this;
+	}
+
+	public PropertiesSilent load(Reader reader) {
+		try {
+			properties.load(reader);
 		} catch (IOException e) {
 			throw new UncheckedIOException(e);
 		}
