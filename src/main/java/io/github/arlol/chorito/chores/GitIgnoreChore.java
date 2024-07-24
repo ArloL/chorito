553e25227fabfd480c3503d258690a40d826e4fd
Arlo O'Keeffe <tiger@k5d.de>
2024-07-24 21:59:29 +0200

    remove usage of gitignores from resource directory

--- src/main/java/io/github/arlol/chorito/chores/GitIgnoreChore.java
+++ src/main/java/io/github/arlol/chorito/chores/GitIgnoreChore.java
@@ -6,7 +6,6 @@ import java.util.function.Function;
 import java.util.stream.Stream;
 
 import io.github.arlol.chorito.tools.ChoreContext;
-import io.github.arlol.chorito.tools.ClassPathFiles;
 import io.github.arlol.chorito.tools.FilesSilent;
 import io.github.arlol.chorito.tools.MyPaths;
 
@@ -56,6 +55,147 @@ public class GitIgnoreChore implements Chore {
 			!/gradle-wrapper.jar
 			!/gradle-wrapper.properties
 
+			""";
+
+	private static String GITIGNORE_ECLIPSE_SETTINGS = """
+			*.prefs
+			!org.eclipse.jdt.core.prefs
+			!org.eclipse.jdt.ui.prefs
+			""";
+
+	private static String GITIGNORE_IDEA_SETTINGS = """
+			# Created by https://www.toptal.com/developers/gitignore/api/intellij
+			# Edit at https://www.toptal.com/developers/gitignore?templates=intellij
+
+			### Intellij ###
+			# Covers JetBrains IDEs: IntelliJ, RubyMine, PhpStorm, AppCode, PyCharm, CLion, Android Studio, WebStorm and Rider
+			# Reference: https://intellij-support.jetbrains.com/hc/en-us/articles/206544839
+
+			# User-specific stuff
+			**/workspace.xml
+			**/tasks.xml
+			**/usage.statistics.xml
+			**/dictionaries
+			**/shelf
+
+			# AWS User-specific
+			**/aws.xml
+
+			# Generated files
+			**/contentModel.xml
+
+			# Sensitive or high-churn files
+			**/dataSources/
+			**/dataSources.ids
+			**/dataSources.local.xml
+			**/sqlDataSources.xml
+			**/dynamic.xml
+			**/uiDesigner.xml
+			**/dbnavigator.xml
+
+			# Gradle
+			**/gradle.xml
+			**/libraries
+
+			# Gradle and Maven with auto-import
+			# When using Gradle or Maven with auto-import, you should exclude module files,
+			# since they will be recreated, and may cause churn.  Uncomment if using
+			# auto-import.
+			artifacts
+			compiler.xml
+			jarRepositories.xml
+			modules.xml
+			*.iml
+			modules
+			# *.iml
+			# *.ipr
+
+			# CMake
+			# cmake-build-*/
+
+			# Mongo Explorer plugin
+			**/mongoSettings.xml
+
+			# File-based project format
+			# *.iws
+
+			# IntelliJ
+			# out/
+
+			# mpeltonen/sbt-idea plugin
+			# .idea_modules/
+
+			# JIRA plugin
+			# atlassian-ide-plugin.xml
+
+			# Cursive Clojure plugin
+			replstate.xml
+
+			# SonarLint plugin
+			sonarlint/
+
+			# Crashlytics plugin (for Android Studio and IntelliJ)
+			# com_crashlytics_export_strings.xml
+			# crashlytics.properties
+			# crashlytics-build.properties
+			# fabric.properties
+
+			# Editor-based Rest Client
+			httpRequests
+
+			# Android studio 3.1+ serialized cache file
+			caches/build_file_checksums.ser
+
+			### Intellij Patch ###
+			# Comment Reason: https://github.com/joeblau/gitignore.io/issues/186#issuecomment-215987721
+
+			# *.iml
+			# modules.xml
+			# misc.xml
+			# *.ipr
+
+			# Sonarlint plugin
+			# https://plugins.jetbrains.com/plugin/7973-sonarlint
+			**/sonarlint/
+
+			# SonarQube Plugin
+			# https://plugins.jetbrains.com/plugin/7238-sonarqube-community-plugin
+			**/sonarIssues.xml
+
+			# Markdown Navigator plugin
+			# https://plugins.jetbrains.com/plugin/7896-markdown-navigator-enhanced
+			**/markdown-navigator.xml
+			**/markdown-navigator-enh.xml
+			**/markdown-navigator/
+
+			# Cache file creation bug
+			# See https://youtrack.jetbrains.com/issue/JBR-2257
+			$CACHE_FILE$
+
+			# CodeStream plugin
+			# https://plugins.jetbrains.com/plugin/12206-codestream
+			codestream.xml
+
+			# Azure Toolkit for IntelliJ plugin
+			# https://plugins.jetbrains.com/plugin/8053-azure-toolkit-for-intellij
+			**/azureSettings.xml
+
+			# End of https://www.toptal.com/developers/gitignore/api/intellij
+
+			encodings.xml
+			jarRepositories.xml
+			misc.xml
+			vcs.xml
+			""";
+
+	private static String GITIGNORE_VSCODE_SETTINGS = """
+			*
+			!.gitignore
+			!settings.json
+			!tasks.json
+			!launch.json
+			!extensions.json
+			!*.code-snippets
 			""";
 
 	private static String GITIGNORE_SUFFIX = """
@@ -175,9 +315,10 @@ public class GitIgnoreChore implements Chore {
 						.map(MyPaths::getParent)
 		).flatMap(Function.identity()).forEach(dir -> {
 			Path settingsGitignore = dir.resolve(".settings/.gitignore");
-			String templateGitignore = ClassPathFiles
-					.readString("eclipse-settings/.gitignore");
-			updateExistingGitignore(settingsGitignore, templateGitignore);
+			String newGitignoreContent = GITIGNORE_PREFIX;
+			newGitignoreContent += "\n" + GITIGNORE_ECLIPSE_SETTINGS;
+			newGitignoreContent += "\n" + GITIGNORE_SUFFIX;
+			updateExistingGitignore(settingsGitignore, newGitignoreContent);
 		});
 	}
 
@@ -189,11 +330,12 @@ public class GitIgnoreChore implements Chore {
 				.map(MyPaths::getParent)
 				.forEach(dir -> {
 					Path settingsGitignore = dir.resolve(".vscode/.gitignore");
-					String templateGitignore = ClassPathFiles
-							.readString("vscode-settings/.gitignore");
+					String newGitignoreContent = GITIGNORE_PREFIX;
+					newGitignoreContent += "\n" + GITIGNORE_VSCODE_SETTINGS;
+					newGitignoreContent += "\n" + GITIGNORE_SUFFIX;
 					updateExistingGitignore(
 							settingsGitignore,
-							templateGitignore
+							newGitignoreContent
 					);
 				});
 	}
@@ -206,11 +348,12 @@ public class GitIgnoreChore implements Chore {
 				.map(MyPaths::getParent)
 				.forEach(dir -> {
 					Path settingsGitignore = dir.resolve(".idea/.gitignore");
-					String templateGitignore = ClassPathFiles
-							.readString("idea-settings/.gitignore");
+					String newGitignoreContent = GITIGNORE_PREFIX;
+					newGitignoreContent += "\n" + GITIGNORE_IDEA_SETTINGS;
+					newGitignoreContent += "\n" + GITIGNORE_SUFFIX;
 					updateExistingGitignore(
 							settingsGitignore,
-							templateGitignore
+							newGitignoreContent
 					);
 				});
 	}
