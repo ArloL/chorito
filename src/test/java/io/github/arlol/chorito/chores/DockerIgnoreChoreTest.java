945075e9179c1432b0b648128b1527514a9e36d3
Arlo O'Keeffe <tiger@k5d.de>
2024-07-28 10:22:06 +0200

    add more tests for DockerIgnoreChore

--- src/test/java/io/github/arlol/chorito/chores/DockerIgnoreChoreTest.java
+++ src/test/java/io/github/arlol/chorito/chores/DockerIgnoreChoreTest.java
@@ -129,7 +129,48 @@ public class DockerIgnoreChoreTest {
 		doit();
 
 		Path dockerIgnore = extension.choreContext().resolve(".dockerignore");
-		assertThat(dockerIgnore).content().contains("compose.yaml");
+		assertThat(dockerIgnore).content()
+				.isEqualTo(
+						"""
+								### Docker ###
+
+								Dockerfile
+								.dockerignore
+								compose.yaml
+
+								# Add custom ignores after this line to be preserved during automated updates
+								"""
+				);
+	}
+
+	@Test
+	public void testAllComposeNames() throws Exception {
+		FilesSilent.touch(extension.choreContext().resolve("Dockerfile"));
+		FilesSilent.touch(extension.choreContext().resolve("compose.yaml"));
+		FilesSilent.touch(extension.choreContext().resolve("compose.yml"));
+		FilesSilent
+				.touch(extension.choreContext().resolve("docker-compose.yml"));
+		FilesSilent
+				.touch(extension.choreContext().resolve("docker-compose.yaml"));
+
+		doit();
+
+		Path dockerIgnore = extension.choreContext().resolve(".dockerignore");
+		assertThat(dockerIgnore).content()
+				.isEqualTo(
+						"""
+								### Docker ###
+
+								Dockerfile
+								.dockerignore
+								compose.yaml
+								compose.yml
+								docker-compose.yml
+								docker-compose.yaml
+
+								# Add custom ignores after this line to be preserved during automated updates
+								"""
+				);
 	}
 
 	@Test
@@ -166,6 +207,22 @@ public class DockerIgnoreChoreTest {
 
 		Path dockerIgnore = extension.choreContext().resolve(".dockerignore");
 		assertThat(dockerIgnore).content().isEqualTo(DEFAULT_MAVEN);
+	}
+
+	@Test
+	public void testPreservingCustomStuff() throws Exception {
+		var dockerignore = OLD_MAVEN + "\n\ndeployment\ncharts\n";
+		FilesSilent.touch(extension.choreContext().resolve("pom.xml"));
+		FilesSilent.writeString(
+				extension.choreContext().resolve(".dockerignore"),
+				dockerignore
+		);
+
+		doit();
+
+		Path dockerIgnore = extension.choreContext().resolve(".dockerignore");
+		assertThat(dockerIgnore).content()
+				.isEqualTo(DEFAULT_MAVEN + "\n\ndeployment\ncharts\n");
 	}
 
 	@Test
