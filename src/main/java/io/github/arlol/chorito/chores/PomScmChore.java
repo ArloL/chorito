b28113172c7d1cb4bffc6c2b5f20fb27234510e1
Arlo O'Keeffe <tiger@k5d.de>
2024-07-28 08:34:23 +0200

    add pom scm data if there is exactly one github.com remote

--- src/main/java/io/github/arlol/chorito/chores/PomScmChore.java
+++ src/main/java/io/github/arlol/chorito/chores/PomScmChore.java
@@ -1,6 +1,7 @@
 package io.github.arlol.chorito.chores;
 
 import java.nio.file.Path;
+import java.util.stream.Collectors;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -15,30 +16,38 @@ public class PomScmChore implements Chore {
 	@Override
 	public ChoreContext doit(ChoreContext context) {
 		Path pom = context.resolve("pom.xml");
-		if (context.hasGitHubRemote() && context.remotes().size() == 1
-				&& FilesSilent.exists(pom)) {
-			String remote = context.remotes().get(0).replace(".git", "");
-
-			Document doc = JsoupSilent
-					.parse(pom, "UTF-8", "", Parser.xmlParser());
-			Element scm = doc.selectFirst("project > scm");
-			if (scm != null) {
-				Element connection = scm.selectFirst("connection");
-				if (connection != null) {
-					connection.text("scm:git:" + remote);
-				}
-				Element developerConnection = scm
-						.selectFirst("developerConnection");
-				if (developerConnection != null) {
-					developerConnection.text("scm:git:" + remote);
-				}
-				Element url = scm.selectFirst("url");
-				if (url != null) {
-					url.text(remote);
-				}
-			}
-			FilesSilent.writeString(pom, doc.outerHtml());
+		if (!FilesSilent.exists(pom)) {
+			return context;
 		}
+		context.remotes()
+				.stream()
+				.filter(s -> s.startsWith("https://github.com"))
+				.map(s -> s.replace(".git", ""))
+				// if there are multiple return null
+				.collect(Collectors.reducing((a, b) -> null))
+				.ifPresent(remote -> {
+
+					Document doc = JsoupSilent
+							.parse(pom, "UTF-8", "", Parser.xmlParser());
+					Element scm = doc.selectFirst("project > scm");
+					if (scm != null) {
+						Element connection = scm.selectFirst("connection");
+						if (connection != null) {
+							connection.text("scm:git:" + remote);
+						}
+						Element developerConnection = scm
+								.selectFirst("developerConnection");
+						if (developerConnection != null) {
+							developerConnection.text("scm:git:" + remote);
+						}
+						Element url = scm.selectFirst("url");
+						if (url != null) {
+							url.text(remote);
+						}
+					}
+					FilesSilent.writeString(pom, doc.outerHtml());
+
+				});
 		return context;
 	}
 
