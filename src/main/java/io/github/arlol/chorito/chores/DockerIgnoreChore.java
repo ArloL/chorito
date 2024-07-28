7d4741b3ea5d88ad36211c1544e52926e6e21f7c
Arlo O'Keeffe <tiger@k5d.de>
2024-07-28 07:39:53 +0200

    update existing .dockerignore and add ignore node_modules

--- src/main/java/io/github/arlol/chorito/chores/DockerIgnoreChore.java
+++ src/main/java/io/github/arlol/chorito/chores/DockerIgnoreChore.java
@@ -25,27 +25,31 @@ public class DockerIgnoreChore implements Chore {
 	private static String DOCKERIGNORE_MAVEN = """
 			### Maven ###
 
-			target/
+			/target/
 			""";
 
 	private static String DOCKERIGNORE_GRADLE = """
 			### Gradle ###
 
-			build/
-			.gradle/
+			/build/
+			/.gradle/
+			""";
+	private static String DOCKERIGNORE_NODE = """
+			### node ###
+
+			/node_modules/
 			""";
 
 	@Override
 	public ChoreContext doit(ChoreContext context) {
 		context.textFiles()
 				.stream()
-				.filter(file -> file.endsWith("Dockerfile"))
+				.filter(
+						file -> file.endsWith("Dockerfile")
+								|| file.endsWith(".dockerignore")
+				)
 				.map(MyPaths::getParent)
-				.forEach(dockerDir -> {
-					Path dockerignore = dockerDir.resolve(".dockerignore");
-					Path pomXml = dockerDir.resolve("pom.xml");
-					Path buildGradle = dockerDir.resolve("build.gradle");
-
+				.forEach(dir -> {
 					String newDockerignoreContent = DOCKERIGNORE_PREFIX;
 					newDockerignoreContent += "\n" + DOCKERIGNORE_DEFAULT;
 					for (String compose : List.of(
@@ -54,18 +58,23 @@ public class DockerIgnoreChore implements Chore {
 							"docker-compose.yml",
 							"docker-compose.yaml"
 					)) {
-						if (FilesSilent.exists(dockerDir.resolve(compose))) {
+						if (FilesSilent.exists(dir.resolve(compose))) {
 							newDockerignoreContent += compose + "\n";
 						}
 					}
-					if (FilesSilent.exists(pomXml)) {
+					if (FilesSilent.anyChildExists(dir, "mvnw", "pom.xml")) {
 						newDockerignoreContent += "\n" + DOCKERIGNORE_MAVEN;
 					}
-					if (FilesSilent.exists(buildGradle)) {
+					if (FilesSilent
+							.anyChildExists(dir, "gradlew", "build.gradle")) {
 						newDockerignoreContent += "\n" + DOCKERIGNORE_GRADLE;
+					}
+					if (FilesSilent.anyChildExists(dir, "package.json")) {
+						newDockerignoreContent += "\n" + DOCKERIGNORE_NODE;
 					}
 					newDockerignoreContent += "\n" + DOCKERIGNORE_SUFFIX;
 
+					Path dockerignore = dir.resolve(".dockerignore");
 					if (!FilesSilent.exists(dockerignore)) {
 						FilesSilent.writeString(
 								dockerignore,
