131958eb7d9137cc59c6bd0f693cd5cee1bde81f
Arlo O'Keeffe <tiger@k5d.de>
2024-08-01 05:51:01 +0200

    extract StreamToStringWriter

---
+++ src/main/java/io/github/arlol/chorito/tools/YamlStreamToStringWriter.java
@@ -0,0 +1,9 @@
+package io.github.arlol.chorito.tools;
+
+import java.io.StringWriter;
+
+import org.snakeyaml.engine.v2.api.StreamDataWriter;
+
+class YamlStreamToStringWriter extends StringWriter implements StreamDataWriter {
+
+}
