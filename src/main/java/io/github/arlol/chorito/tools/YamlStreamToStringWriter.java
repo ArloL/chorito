7abdec387210bccb4a4e62d891a3f608fe70446c
Arlo O'Keeffe <tiger@k5d.de>
2024-08-01 07:33:08 +0200

    formatting

--- src/main/java/io/github/arlol/chorito/tools/YamlStreamToStringWriter.java
+++ src/main/java/io/github/arlol/chorito/tools/YamlStreamToStringWriter.java
@@ -4,6 +4,7 @@ import java.io.StringWriter;
 
 import org.snakeyaml.engine.v2.api.StreamDataWriter;
 
-class YamlStreamToStringWriter extends StringWriter implements StreamDataWriter {
+class YamlStreamToStringWriter extends StringWriter
+		implements StreamDataWriter {
 
 }
