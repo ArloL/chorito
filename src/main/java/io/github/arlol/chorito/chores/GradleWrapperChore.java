014e4d923c591218459cb05f566019399f665f99
Arlo O'Keeffe <tiger@k5d.de>
2024-08-15 16:07:13 +0200

    update gradle wrapper to 8.10

--- src/main/java/io/github/arlol/chorito/chores/GradleWrapperChore.java
+++ src/main/java/io/github/arlol/chorito/chores/GradleWrapperChore.java
@@ -19,7 +19,7 @@ public class GradleWrapperChore implements Chore {
 	private static String DEFAULT_PROPERTIES = """
 			distributionBase=GRADLE_USER_HOME
 			distributionPath=wrapper/dists
-			distributionUrl=https\\://services.gradle.org/distributions/gradle-8.9-all.zip
+			distributionUrl=https\\://services.gradle.org/distributions/gradle-8.10-all.zip
 			networkTimeout=10000
 			validateDistributionUrl=true
 			zipStoreBase=GRADLE_USER_HOME
@@ -46,7 +46,7 @@ public class GradleWrapperChore implements Chore {
 									"./gradlew",
 									"wrapper",
 									"--gradle-version",
-									"8.9",
+									"8.10",
 									"--distribution-type",
 									"all",
 									"--no-daemon"
@@ -59,7 +59,7 @@ public class GradleWrapperChore implements Chore {
 									"./gradlew",
 									"wrapper",
 									"--gradle-version",
-									"8.9",
+									"8.10",
 									"--distribution-type",
 									"all",
 									"--no-daemon"
