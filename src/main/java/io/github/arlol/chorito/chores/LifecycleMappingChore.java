5e9071469cf4f9499926e0160bed839758b37f6f
Arlo O'Keeffe <tiger@k5d.de>
2024-08-22 09:54:17 +0200

    refactor LifecycleMappingChore

--- src/main/java/io/github/arlol/chorito/chores/LifecycleMappingChore.java
+++ src/main/java/io/github/arlol/chorito/chores/LifecycleMappingChore.java
@@ -1,10 +1,10 @@
 package io.github.arlol.chorito.chores;
 
-import java.nio.file.Path;
-
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
 
 import io.github.arlol.chorito.tools.ChoreContext;
 import io.github.arlol.chorito.tools.FilesSilent;
@@ -14,25 +14,29 @@ public class LifecycleMappingChore implements Chore {
 
 	@Override
 	public ChoreContext doit(ChoreContext context) {
-		Path pomXml = context.resolve("pom.xml");
-		if (FilesSilent.exists(pomXml)) {
-			Document doc = JsoupSilent
-					.parse(pomXml, "UTF-8", "", Parser.xmlParser());
+		context.textFiles()
+				.stream()
+				.filter(file -> file.endsWith("pom.xml"))
+				.forEach(pomXml -> {
+					Document doc = JsoupSilent
+							.parse(pomXml, "UTF-8", "", Parser.xmlParser());
 
-			Element lifecycleMappingPlugin = doc.select(
-					"plugin:has(groupId:containsWholeOwnText(org.eclipse.m2e)):has(artifactId:containsWholeOwnText(lifecycle-mapping))"
-			).first();
-			if (lifecycleMappingPlugin == null) {
-				return context;
-			}
-			while (!(lifecycleMappingPlugin
-					.previousSibling() instanceof Element)) {
-				lifecycleMappingPlugin.previousSibling().remove();
-			}
-			lifecycleMappingPlugin.remove();
-
-			FilesSilent.writeString(pomXml, doc.outerHtml());
-		}
+					Elements lifecycleMappingPlugins = doc.select(
+							"plugin:has(groupId:containsWholeOwnText(org.eclipse.m2e)):has(artifactId:containsWholeOwnText(lifecycle-mapping))"
+					);
+					for (Element lifecycleMappingPlugin : lifecycleMappingPlugins) {
+						while (!(lifecycleMappingPlugin
+								.previousSibling() instanceof Element)) {
+							Node previousSibling = lifecycleMappingPlugin
+									.previousSibling();
+							if (previousSibling != null) {
+								previousSibling.remove();
+							}
+						}
+						lifecycleMappingPlugin.remove();
+					}
+					FilesSilent.writeString(pomXml, doc.outerHtml());
+				});
 		return context;
 	}
 
