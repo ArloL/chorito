2a385cd74c7d773b024e29bd08f681e58eeab85b
Arlo O'Keeffe <tiger@k5d.de>
2025-03-02 00:38:07 +0100

    simplify ExecutableFlagger

--- src/main/java/io/github/arlol/chorito/tools/ExecutableFlagger.java
+++ src/main/java/io/github/arlol/chorito/tools/ExecutableFlagger.java
@@ -1,8 +1,5 @@
 package io.github.arlol.chorito.tools;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.PosixFileAttributeView;
 import java.nio.file.attribute.PosixFilePermission;
@@ -13,53 +10,40 @@ public abstract class ExecutableFlagger {
 	}
 
 	public static void makeExecutableIfPossible(Path path) {
-		try {
-			PosixFileAttributeView view = Files
-					.getFileAttributeView(path, PosixFileAttributeView.class);
-			if (view != null) {
-				var permissions = view.readAttributes().permissions();
-				permissions.add(PosixFilePermission.OWNER_EXECUTE);
-				permissions.add(PosixFilePermission.GROUP_EXECUTE);
-				permissions.add(PosixFilePermission.OTHERS_EXECUTE);
-				view.setPermissions(permissions);
-			}
-		} catch (IOException e) {
-			throw new UncheckedIOException(e);
+		var view = FilesSilent
+				.getFileAttributeView(path, PosixFileAttributeView.class);
+		if (view != null) {
+			var permissions = FilesSilent.getPosixFilePermissions(view);
+			permissions.add(PosixFilePermission.OWNER_EXECUTE);
+			permissions.add(PosixFilePermission.GROUP_EXECUTE);
+			permissions.add(PosixFilePermission.OTHERS_EXECUTE);
+			FilesSilent.setPosixFilePermissions(view, permissions);
 		}
 	}
 
 	public static void makeNotExecutableIfPossible(Path path) {
-		try {
-			PosixFileAttributeView view = Files
-					.getFileAttributeView(path, PosixFileAttributeView.class);
-			if (view != null) {
-				var permissions = view.readAttributes().permissions();
-				permissions.remove(PosixFilePermission.OWNER_EXECUTE);
-				permissions.remove(PosixFilePermission.GROUP_EXECUTE);
-				permissions.remove(PosixFilePermission.OTHERS_EXECUTE);
-				view.setPermissions(permissions);
-			}
-		} catch (IOException e) {
-			throw new UncheckedIOException(e);
+		var view = FilesSilent
+				.getFileAttributeView(path, PosixFileAttributeView.class);
+		if (view != null) {
+			var permissions = FilesSilent.getPosixFilePermissions(view);
+			permissions.remove(PosixFilePermission.OWNER_EXECUTE);
+			permissions.remove(PosixFilePermission.GROUP_EXECUTE);
+			permissions.remove(PosixFilePermission.OTHERS_EXECUTE);
+			FilesSilent.setPosixFilePermissions(view, permissions);
 		}
 	}
 
 	public static boolean isExecutable(Path path) {
-		try {
-			PosixFileAttributeView view = Files
-					.getFileAttributeView(path, PosixFileAttributeView.class);
-			if (view != null) {
-				var permissions = view.readAttributes().permissions();
-				if (permissions.contains(PosixFilePermission.OWNER_EXECUTE)
-						|| permissions
-								.contains(PosixFilePermission.GROUP_EXECUTE)
-						|| permissions
-								.contains(PosixFilePermission.OTHERS_EXECUTE)) {
-					return true;
-				}
+		var view = FilesSilent
+				.getFileAttributeView(path, PosixFileAttributeView.class);
+		if (view != null) {
+			var permissions = FilesSilent.getPosixFilePermissions(view);
+			if (permissions.contains(PosixFilePermission.OWNER_EXECUTE)
+					|| permissions.contains(PosixFilePermission.GROUP_EXECUTE)
+					|| permissions
+							.contains(PosixFilePermission.OTHERS_EXECUTE)) {
+				return true;
 			}
-		} catch (IOException e) {
-			throw new UncheckedIOException(e);
 		}
 		return false;
 	}
