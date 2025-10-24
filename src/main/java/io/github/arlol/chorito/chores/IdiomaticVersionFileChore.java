5fd7dbbdf8e7e9dbb2934db6a9244d5f67643eed
Arlo O'Keeffe <tiger@k5d.de>
2025-10-24 16:03:11 +0200

    feature: create tool-versions file instead of .java-version

--- src/main/java/io/github/arlol/chorito/chores/IdiomaticVersionFileChore.java
+++ src/main/java/io/github/arlol/chorito/chores/IdiomaticVersionFileChore.java
@@ -11,14 +11,15 @@ public class IdiomaticVersionFileChore implements Chore {
 	@Override
 	public ChoreContext doit(ChoreContext context) {
 		DirectoryStreams.javaDirs(context).forEach(javaDir -> {
-			Path javaVersion = javaDir.resolve(".java-version");
-			if (!FilesSilent.exists(javaVersion)) {
-				var version = "temurin-25\n";
+			FilesSilent.deleteIfExists(javaDir.resolve(".java-version"));
+			Path toolVersions = javaDir.resolve(".tool-versions");
+			if (!FilesSilent.exists(toolVersions)) {
+				var version = "java temurin-25\n";
 				if (FilesSilent.readString(javaDir.resolve("pom.xml"))
 						.contains("native-maven-plugin")) {
-					version = "graalvm-community-25.0.0\n";
+					version = "java graalvm-community-25\n";
 				}
-				FilesSilent.writeString(javaVersion, version);
+				FilesSilent.writeString(toolVersions, version);
 			}
 
 		});
