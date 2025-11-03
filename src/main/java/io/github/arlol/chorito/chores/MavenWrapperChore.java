463f9e6660c35c3c190ba8d40523c60580b4bcd5
Arlo O'Keeffe <tiger@k5d.de>
2025-11-03 21:28:29 +0100

    chore: update maven

--- src/main/java/io/github/arlol/chorito/chores/MavenWrapperChore.java
+++ src/main/java/io/github/arlol/chorito/chores/MavenWrapperChore.java
@@ -18,26 +18,10 @@ public class MavenWrapperChore implements Chore {
 			.getLogger(MavenWrapperChore.class);
 
 	private static String DEFAULT_PROPERTIES = """
-			# Licensed to the Apache Software Foundation (ASF) under one
-			# or more contributor license agreements.  See the NOTICE file
-			# distributed with this work for additional information
-			# regarding copyright ownership.  The ASF licenses this file
-			# to you under the Apache License, Version 2.0 (the
-			# "License"); you may not use this file except in compliance
-			# with the License.  You may obtain a copy of the License at
-			#
-			#   http://www.apache.org/licenses/LICENSE-2.0
-			#
-			# Unless required by applicable law or agreed to in writing,
-			# software distributed under the License is distributed on an
-			# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-			# KIND, either express or implied.  See the License for the
-			# specific language governing permissions and limitations
-			# under the License.
-			wrapperVersion=3.3.2
+			wrapperVersion=3.3.4
 			distributionType=bin
-			distributionUrl=https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.9.8/apache-maven-3.9.8-bin.zip
-			wrapperUrl=https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.3.2/maven-wrapper-3.3.2.jar
+			distributionUrl=https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.9.11/apache-maven-3.9.11-bin.zip
+			wrapperUrl=https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.3.4/maven-wrapper-3.3.4.jar
 			""";
 
 	@Override
@@ -53,12 +37,12 @@ public class MavenWrapperChore implements Chore {
 							.resolve(".mvn/wrapper/maven-wrapper.properties");
 					if (!FilesSilent.exists(wrapper)
 							|| !FilesSilent.exists(wrapperJar)) {
-						LOG.info("Running mvn wrapper:3.3.2:wrapper");
+						LOG.info("Running mvn wrapper:3.3.4:wrapper");
 						context.newProcessBuilder(
 								"mvn",
 								"-N",
-								"wrapper:3.3.2:wrapper",
-								"-Dmaven=3.9.8",
+								"wrapper:3.3.4:wrapper",
+								"-Dmaven=3.9.11",
 								"-Dtype=bin"
 						)
 								.inheritIO()
@@ -75,12 +59,12 @@ public class MavenWrapperChore implements Chore {
 						String content = FilesSilent
 								.readString(wrapperProperties);
 						if (!DEFAULT_PROPERTIES.equals(content)) {
-							LOG.info("Running ./mvnw wrapper:3.3.2:wrapper");
+							LOG.info("Running ./mvnw wrapper::wrapper");
 							context.newProcessBuilder(
 									"./mvnw",
 									"-N",
-									"wrapper:3.3.2:wrapper",
-									"-Dmaven=3.9.8",
+									"wrapper:3.3.4:wrapper",
+									"-Dmaven=3.9.11",
 									"-Dtype=bin"
 							)
 									.inheritIO()
