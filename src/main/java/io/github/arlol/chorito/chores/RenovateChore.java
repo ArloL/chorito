c61cc59407f88bcaa249cd6cb8a4f6ba397d9aeb
Arlo O'Keeffe <tiger@k5d.de>
2026-03-04 21:33:45 +0100

    add JsonBuilder

--- src/main/java/io/github/arlol/chorito/chores/RenovateChore.java
+++ src/main/java/io/github/arlol/chorito/chores/RenovateChore.java
@@ -2,12 +2,9 @@ package io.github.arlol.chorito.chores;
 
 import java.nio.file.Path;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import io.github.arlol.chorito.tools.ChoreContext;
 import io.github.arlol.chorito.tools.FilesSilent;
-import io.github.arlol.chorito.tools.Jsons;
+import io.github.arlol.chorito.tools.JsonBuilder;
 
 public class RenovateChore implements Chore {
 
@@ -21,51 +18,45 @@ public class RenovateChore implements Chore {
 		}
 		if (FilesSilent.exists(renovateJson5)) {
 			var content = FilesSilent.readString(renovateJson5);
-			Jsons.parse(content).ifPresent(node -> {
-				if (!(node instanceof ObjectNode root)) {
-					return;
-				}
-
-				JsonNode releaseAge = root.get("minimumReleaseAge");
-				if (releaseAge != null
-						&& "4 days".equals(releaseAge.asText())) {
-					root.put("minimumReleaseAge", "7 days");
-				}
-
-				if (!root.has("labels")) {
-					root.putArray("labels").add("dependencies");
-					root.putArray("addLabels").add("{{manager}}");
-				}
-
-				JsonNode vulnAlerts = root.get("vulnerabilityAlerts");
-				if (vulnAlerts instanceof ObjectNode vulnNode
-						&& !vulnNode.has("addLabels")) {
-					vulnNode.putArray("addLabels").add("security");
-				}
-
-				var newContent = Jsons.asString(root);
-				if (!newContent.equalsIgnoreCase(content)) {
-					FilesSilent.writeString(renovateJson5, newContent);
-				}
-			});
+			var newContent = JsonBuilder.wrap(content)
+					.migrateString("minimumReleaseAge", "4 days", "7 days")
+					.ifAbsent(
+							"labels",
+							root -> root.array("labels", "dependencies")
+									.array("addLabels", "{{manager}}")
+					)
+					.ifObjectPresent(
+							"vulnerabilityAlerts",
+							v -> v.ifAbsent(
+									"addLabels",
+									a -> a.array("addLabels", "security")
+							)
+					)
+					.asString();
+			if (!newContent.equalsIgnoreCase(content)) {
+				FilesSilent.writeString(renovateJson5, newContent);
+			}
 		} else if (context.remotes()
 				.stream()
-				.anyMatch(s -> s.startsWith("https://github.com"))) {
-			ObjectNode root = Jsons.objectMapper().createObjectNode();
-			root.put(
-					"$schema",
-					"https://docs.renovatebot.com/renovate-schema.json"
-			);
-			root.putArray("extends").add("config:recommended");
-			root.putArray("labels").add("dependencies");
-			root.putArray("addLabels").add("{{manager}}");
-			root.put("minimumReleaseAge", "7 days");
-			root.putArray("schedule").add("on the 20th day of the month");
-			ObjectNode vulnAlerts = root.putObject("vulnerabilityAlerts");
-			vulnAlerts.putArray("schedule").add("at any time");
-			vulnAlerts.put("minimumReleaseAge", "0 days");
-			vulnAlerts.putArray("addLabels").add("security");
-			FilesSilent.writeString(renovateJson5, Jsons.asString(root));
+				.anyMatch(r -> r.startsWith("https://github.com"))) {
+			var content = JsonBuilder.object()
+					.put(
+							"$schema",
+							"https://docs.renovatebot.com/renovate-schema.json"
+					)
+					.array("extends", "config:recommended")
+					.array("labels", "dependencies")
+					.array("addLabels", "{{manager}}")
+					.put("minimumReleaseAge", "7 days")
+					.array("schedule", "on the 20th day of the month")
+					.object(
+							"vulnerabilityAlerts",
+							v -> v.array("schedule", "at any time")
+									.put("minimumReleaseAge", "0 days")
+									.array("addLabels", "security")
+					)
+					.asString();
+			FilesSilent.writeString(renovateJson5, content);
 			context.setDirty();
 		}
 		return context;
